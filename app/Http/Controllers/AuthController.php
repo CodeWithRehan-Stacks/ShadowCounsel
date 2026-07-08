52d0a22c92bf8c3962de7d7b21d9b5540e154cda
Muhammad Rehan <rehan@byklabs.com>
2026-07-08 18:51:49 +0500

    feat: implement AuthController to manage user registration, authentication, and session handling

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -32,7 +32,7 @@ class AuthController extends Controller
         if (Auth::attempt($credentials, $remember)) {
             $request->session()->regenerate();
 
-            return redirect()->intended('dashboard')->with('success', 'You are logged in.');
+            return redirect()->intended(route('dashboard'))->with('success', 'You are logged in.');
         }
 
         return back()->withErrors([
